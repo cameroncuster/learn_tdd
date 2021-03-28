6c050010583c8729ea097946e477552b10b62d07
101061877 <cameron.custer@mines.sdsmt.edu>
2021-03-27 23:33:48 -0600

    udpated for point tests, ready to merge with master

--- BranchMerge/testCasesPoint.cpp
+++ BranchMerge/testCasesPoint.cpp
@@ -1,1 +1,58 @@
 #include "unitTestTools.h"
+
+using namespace std;
+
+TEST_CASE( "Point Constructor Comparison Tested with Random Values" )
+{
+	int i;
+	for( i = 0; i < capacity; i++ )
+	{
+		pair<Point, PointTest> p = constructRandomPointPair( bound );
+
+		REQUIRE( p.first == p.second );
+	}
+}
+
+TEST_CASE( "Point Distance Comparison Tested with Random Values" )
+{
+	int i;
+	for( i = 0; i < capacity; i++ )
+	{
+		pair<Point, PointTest> p1 = constructRandomPointPair( bound );
+		pair<Point, PointTest> p2 = constructRandomPointPair( bound );
+
+		REQUIRE( p1.first.distance( p2.first ) == p1.second.distance( p2.second ) );
+		REQUIRE( p2.first.distance( p1.first ) == p2.second.distance( p1.second ) );
+	}
+}
+
+TEST_CASE( "Point operator( - ) Comparison Tested with Random Values" )
+{
+	int i;
+	for( i = 0; i < capacity; i++ )
+	{
+		pair<Point, PointTest> p1 = constructRandomPointPair( bound );
+		pair<Point, PointTest> p2 = constructRandomPointPair( bound );
+
+		pair<Point, PointTest> p3 = { p2.first - p1.first, p2.second - p1.second };
+
+		REQUIRE( p3.first == p3.second );
+	}
+}
+
+TEST_CASE( "Point operator( << ) Comparison Tested with Random Values" )
+{
+	int i;
+	stringstream sout;
+	stringstream soutTest;
+	for( i = 0; i < capacity; i++ )
+	{
+		pair<Point, PointTest> p1 = constructRandomPointPair( bound );
+		pair<Point, PointTest> p2 = constructRandomPointPair( bound );
+
+		sout << p1.first << p2.first;
+		soutTest << p1.second << p2.second;
+
+		REQUIRE( sout.str( ) == soutTest.str( ) );
+	}
+}
--- BranchMerge/unitTestTools.h
+++ BranchMerge/unitTestTools.h
@@ -1,5 +1,6 @@
 #include "../catch.hpp"
 #include "point.h"
+#include "pointTest.h"
 #include "cube.h"
 #include "ellipsoid.h"
 #include "prism.h"
@@ -7,5 +8,9 @@
 
 using namespace std;
 
-const constexpr int capacity = 9001;
+const constexpr int capacity = 1000;
 const constexpr int bound = 900001;
+
+vector<double> randCoord( const int );
+pair<Point, PointTest> constructRandomPointPair( const int );
+bool operator==( Point &lhs, PointTest &rhs );
